cd9bf8a53229913e3e83224b531d18b4aa154768
Aamir Shah <aamir.shah.92@gmail.com>
2016-11-09 00:20:11 +0500

    Updated Hello World to Reset and increment counter on tap on button

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import Metal
 
 class ViewController: UIViewController {
-
+    var num: Int = 0
+    @IBOutlet weak var label: UILabel!
+    @IBAction func buttonTapped(_ sender: Any) {
+        num = num + 1
+        label.text = "Hello World " + String(num) + " !"
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        num = 1
+        label.text = "Label"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
